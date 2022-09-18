a5e09a2bc6d0a51a86ca70d3184741175e79704d
han <afaideen@gmail.com>
2022-09-18 18:01:45 +0800

    updated

--- run.py
+++ run.py
@@ -4,3 +4,9 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+    ##use '0.0.0.0' to access the server from external ip
+    # app.run(host="0.0.0.0", port=5000, ssl_context=('cert.pem', 'key.pem'))
+    # app.run(host="127.0.0.1", port=5000)
+    # app.run(host="0.0.0.0")
+    # app.run(host="0.0.0.0", port=5000)
+    # app.run(host="192.168.137.1", port=5000)
