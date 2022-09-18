675aa216abe283bc6279d15f150d41f94058dc6b
han <afaideen@gmail.com>
2022-09-18 17:40:33 +0800

    updated

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flaskapp.config import Config
 from flaskapp.main.routes import main
 from flaskapp.users.routes import users
+from flaskapp.errors.handlers import errors
 
 
 def create_app(config_class=Config):
@@ -11,4 +12,5 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
     app.register_blueprint(main)
     app.register_blueprint(users)
+    app.register_blueprint(errors)
     return app
